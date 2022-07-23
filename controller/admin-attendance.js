aea9ba707ba639efd0894c7e9863333bfed31373
sheikh ariful <sheikharifulislam770@gmail.com>
2022-07-24 04:13:55 +0600

    add date fns library

--- controller/admin-attendance.js
+++ controller/admin-attendance.js
@@ -5,7 +5,7 @@ const getEnable = async (req, res, next) => {
     try {
         const running = await AdminAttendance.findOne({ status: "RUNNING" });
         if (running) {
-            throw error("One time Sheet alredy running", 400);
+            throw error("TimeSheet alredy running", 400);
         }
         const attendance = new AdminAttendance({});
         await attendance.save();
@@ -15,9 +15,22 @@ const getEnable = async (req, res, next) => {
     }
 };
 
+const getStatus = async (req, res, next) => {
+    try {
+        const running = await AdminAttendance.findOne({ status: "RUNNING" });
+        if (!running) {
+            throw error("Not running", 400);
+        }
+        return res.status(200).json(running);
+    } catch (err) {
+        next(err);
+    }
+};
+
 const getDisable = (req, res, next) => {};
 
 module.exports = {
     getEnable,
+    getStatus,
     getDisable,
 };
--- routes/admin-attendance.js
+++ routes/admin-attendance.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
-const { getEnable, getDisable } = require("../controller/admin-attendance");
+const {
+    getEnable,
+    getDisable,
+    getStatus,
+} = require("../controller/admin-attendance");
 
 router.get("/enable", getEnable);
+router.get("/status", getStatus);
 router.get("/disable", getDisable);
 
 module.exports = router;
